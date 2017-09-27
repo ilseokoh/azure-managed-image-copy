f709f83d19b805a3259b7cfb983d8cd41f086dd2
오일석 <onestone@live.co.kr>
2017-09-27 09:42:17 +0900

    Azure CLI version is completed

---
+++ CLI2/azure-image.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# azure CLI 2.0 installation: https://docs.microsoft.com/ko-kr/cli/azure/install-azure-cli?view=azure-cli-latest
+targetResourceGroupName="TargetGroup"
+targetStorageAccountName="targettmp"
+targetImageStorageContainerName="images"
+targetImageName="TargetImage"
+# 포탈에서 Export 메뉴로 만든 SAS URL 
+srcSasUrl="https://md-g23zjjmrllxj.blob.core.windows.net/zx45ct4r2pdv/abcd?sv=2016-05-31&sr=b&si=0e9a41b9-b058-4fc6-b2f2-01cdc085b449&sig=0Sn8Zxo9Jf1yW6wWdYDLucLs8FS%2BA5tjeJpxzSYu0IM%3D"
+
+# 다른 지역/다른 구독에 있는 Azure Storage Account
+targetStorageAccountKey=$(az storage account keys list -g $targetResourceGroupName --account-name $targetStorageAccountName --query "[:1].value" -o tsv)
+storageSasToken=$(az storage account generate-sas --expiry 2017-10-01'T'12:00'Z' --permissions aclrpuw --resource-types sco --services b --https-only --account-name $targetStorageAccountName --account-key $targetStorageAccountKey -o tsv)
+
+# Container 생성 
+echo "create a new container"
+#az storage container create -n $targetImageStorageContainerName --account-name $targetStorageAccountName --sas-token $storageSasToken
+
+# 복사 시작
+echo "Copy is Started"
+imageBlobName="$targetImageName-osdisk.vhd"
+copyId=$(az storage blob copy start --source-uri $srcSasUrl --destination-blob $imageBlobName --destination-container $targetImageStorageContainerName --sas-token $storageSasToken --account-name $targetStorageAccountName)
+
+# 복사 진행 모니터링
+percent=0
+while [ $percent -lt 100 ] 
+do
+    status=$(az storage blob show --container-name $targetImageStorageContainerName -n $imageBlobName --account-name $targetStorageAccountName --sas-token $storageSasToken --query "properties.copy.progress")
+    status=$(echo "$status" | sed -e 's/^"//' -e 's/"$//')
+    echo $status
+    item=$(echo $status | cut -f1 -d "/")
+    total=$(echo $status | cut -f2 -d "/")
+    echo $item
+    echo $total
+    percent=$(awk "BEGIN { pc=100*${item}/${total}; i=int(pc); print (pc-i<0.5)?i:i+1 }")
+    echo -ne $percent
+    sleep 1
+done
+echo "Copy Done"
+
+
+# 복사한 VHD 파일에서 Managed Image를 만든다. 
+hostname=$(az storage account show --name $targetStorageAccountName --resource-group $targetResourceGroupName --query "primaryEndpoints.blob")
+hostname=$(echo "$hostname" | sed -e 's/^"//' -e 's/"$//')
+imageVhdUrl="$hostname$targetImageStorageContainerName/$imageBlobName"
+echo "VHD Url: $imageVhdUrl"
+az image create --resource-group $targetResourceGroupName --name $targetImageName --source $imageVhdUrl --os-type Linux
+echo "Finished successfully"
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# azure-managed-image-copy
+# Azure VM 이미지 
+Azure Portal, Powershell, Azure CLI 2.0을 통해서 VM을 통째로 이미지(Managed Image)로 만들 수 있다. [Azure에서 일반화된 VM의 관리 이미지 만들기 (윈도우 서버, PowerShell)](https://docs.microsoft.com/ko-kr/azure/virtual-machines/windows/capture-image-resource), [가상 컴퓨터 또는 VHD의 이미지를 만드는 방법 (리눅스 Azure CLI 2.0)](https://docs.microsoft.com/ko-kr/azure/virtual-machines/linux/capture-image)문서를 참조하면 이미지를 생성할 수 있고 같은 구독과 지역에서 그 이미지로 다시 VM을 만들 수 있다. 이렇게 하면 VM에 설치되어 있는 미들웨어, 프레임워크, 애플리케이션까지 모두 그대로 복사하듯이 VM을 만들 수 있다. 
+
+하지만 2017년 9월 15일 현재 이렇게 만든 이미지를 다른 구독이나 다른 지역으로 복사하는 기능이 제공되지 않는다. 현재 개발중.
+
+# VM을 그대로 다른 지역이나 다른 구독으로 옮기고 싶다. 
+이미지 Export를 이용하면 조금 복잡하지만 가능하다. 순서는 아래와 같다. 
+
+1. Azure Portal에서 Image생성 (포탈)
+1. Azure Portal에서 Image를 복사할 수 있도록 스토리지 SAS URL 생성 (포탈 Export 기능)
+1. URL을 이용하여 소스 구독/지역에서 타겟 구독/지역으로 복사 (Powershell 또는 Azure CLI 2.0)
+1. 복사된 VHD 파일을 이미지로 변환 (Powershell 또는 Azure CLI 2.0)
+1. 이미지에서 VM 생성(포탈)
+
+# 유튜브 동영상 링크 
+전체 프로세스를 동영상으로 제공합니다. 
+
+# 참고자료 
